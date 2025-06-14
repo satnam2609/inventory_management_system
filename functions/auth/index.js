44a9130f428d5507f5813a7347dd63d158847128
satnam2609 <satnamm143@gmail.com>
2025-06-14 19:50:46 +0530

    Cleanup auth function

--- functions/auth/index.js
+++ functions/auth/index.js
@@ -10,10 +10,3 @@ export const registerApi = async (userName, email, password) => {
   return res.data;
 };
 
-export const currentUser = async (email) => {
-  const res = await axios.post("http://localhost:3000/api/auth/current", {
-    email,
-  });
-
-  return res.data.message;
-};
